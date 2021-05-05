ed0695ff29930463ab4bc9edb7a0a1c5ddbb204e
Akogwu24 <akogwu.emmanuel24@gmail.com>
2021-05-05 12:50:32 +0100

    nav bar done

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import clapway from '../assets/icons/clapway.png';
+
+const Logo = () => {
+  return (
+    <div className=''>
+      <img src={clapway} alt='clapwy-logo' className='w-1/4 my-20 ml-40' />
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { FiChevronDown } from 'react-icons/fi';
+import { HiOutlineSearch } from 'react-icons/hi';
+const Navbar = () => {
+  const listItems = ['SCIENCE', 'CRYPTO', 'GAMES', 'CANNABIS', 'SERVICES'];
+
+  return (
+    <nav className='flex justify-around items-center font-bold bg-black text-white'>
+      <ul className='dropdown'>
+        <li className='dropbtn flex items-center'>
+          <a href='/'>ALL</a>
+          <FiChevronDown color='white font-bold ml-2' />
+        </li>
+        <li className='dropdown-content text-black shadow-md'>
+          <a href='/'>NEWS</a>
+          <a href='/'>TIPS</a>
+          <a href='/'>TOP LISTS</a>
+          <a href='/'>REVIEWS</a>
+        </li>
+      </ul>
+
+      <ul className='flex justify-between items-center w-2/5'>
+        {listItems.map((item) => (
+          <li className='middle'>
+            <a href='/'> {item} </a>
+          </li>
+        ))}
+      </ul>
+
+      <ul className='dropdown flex'>
+        <li className='dropbtn flex items-center'>
+          <a href='/'>OTHER</a>
+          <FiChevronDown className='ml-2' />
+        </li>
+        <li className='dropdown-content text-black shadow-md'>
+          <a href='/'>WEBSITE</a>
+          <a href='/'>BRAND</a>
+          <a href='/'>TECH</a>
+          <a href='/'>SOCILA</a>
+          <a href='/'>ADULT</a>
+        </li>
+      </ul>
+      <ul>
+        <a href='/' className='mr-40'>
+          FFEATURED
+        </a>
+      </ul>
+      <HiOutlineSearch className='ml-40' />
+    </nav>
+  );
+};
+
+export default Navbar;
